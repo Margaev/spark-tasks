dd9a0743776826c4aa6a6a5b475ba778d662c763
Margaev <andreymargaev@gmail.com>
2021-01-19 18:05:01 +0300

    Erroneous records task reformated

--- motel_tasks.py
+++ motel_tasks.py
@@ -8,17 +8,11 @@ BIDS_PATH = os.path.join(INPUT_DIR, 'bids.txt')
 EXCHANGE_RATE_PATH = os.path.join(INPUT_DIR, 'exchange_rate.txt')
 MOTELS_PATH = os.path.join(INPUT_DIR, 'motels.txt')
 
-if __name__ == '__main__':
-    spark = SparkSession \
-        .builder \
-        .appName('Motels Task') \
-        .getOrCreate()
 
-    bids_df = spark.read.text(BIDS_PATH)
-
+def filter_erroneous_records(bids_df):
     bids_df = bids_df.select(split(bids_df['value'], ',', 18).alias('arr'))
 
-    column_names = ["MotelID", "BidDate", "HU", "UK",  "NL", "US", "MX", "AU",
+    column_names = ["MotelID", "BidDate", "HU", "UK", "NL", "US", "MX", "AU",
                     "CA", "CN", "KR", "BE", "I", "JP", "IN", "HN", "GY", "DE"]
 
     select_statement = [col('arr').getItem(i).alias(column_names[i]) for i in range(18)]
@@ -27,12 +21,7 @@ if __name__ == '__main__':
         *select_statement
     )
 
-    erroneous_bids = bids_df.select(
-        '*'
-    ).where(col('HU').startswith('ERROR_'))
-
-    erroneous_bids = erroneous_bids.withColumn('BidDate', to_timestamp(col('BidDate'), 'H-d-M-y'))
-    erroneous_bids = erroneous_bids.withColumn('BidDate', date_format('BidDate', 'H-d-M-y'))
+    erroneous_bids = bids_df.select('*').where(col('HU').startswith('ERROR_'))
 
     grouped_erroneous_bids = erroneous_bids.groupby(['BidDate', 'HU'])
     erroneous_bids_count = grouped_erroneous_bids.count()
@@ -41,3 +30,19 @@ if __name__ == '__main__':
         .withColumn('count', col('count').astype('string'))
 
     erroneous_bids_count.write.mode('overwrite').csv('bids_error_count_csv')
+
+    return bids_df.select('*').where(~col('HU').startswith('ERROR_'))
+
+
+def main():
+    spark = SparkSession \
+        .builder \
+        .appName('Motels Task') \
+        .getOrCreate()
+
+    bids_df = spark.read.text(BIDS_PATH)
+    cleared_bids_df = filter_erroneous_records(bids_df)
+
+
+if __name__ == '__main__':
+    main()
